a8940a1db5edac2d434efc75872e7ca87f7e411d
jkussoy <s22127003@student.unklab.ac.id>
2025-06-03 12:43:20 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// console.log("Hello World");
+
+//Javascript Variable
+// let firstName = "John"; //Camel case
+// let first_name = "John"; //Snake case
+// let FirstName = "John"; //Pascal case
+
+//Tipe Data
+//Const fullName = "John Doe"
+// fullName = "Bob"; //error
+
+// let fullName = "John Doe"; // String
+// let age = 30; //Number bisa bilangan bulat atau desimal
+// let isMarried = false; //Boolean
+// let address; //undefined
+// console.log(fullName);
+// console.log(age);
+// console.log(isMarried);
+
+//Javascript Operator
+// let bil1 = 10;
+// let bil2 = 3;
+
+//Artimethic
+// console.log(bil1 + bil2);
+// console.log(bil1 - bil2);
+// console.log(bil1 / bil2);
+// console.log(bil1 * bil2);
+// console.log(bil1 % bil2);
+// console.log(bil1 ** bil2);
+
+//Assignment Operator
+// let x = 10;
+// // x = x + 1;
+// x++;
+// console.log(x);
+
+//string operator
+// let firstName = "John";
+// let lastName = "Doe";
+// console.log(firstName + " " + lastName);
+
+//Equality Operator
+let x = 10;
+let y = "10";
+console.log(x == y); //true
+console.log(x === y); // false
