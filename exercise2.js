82e59f9487529e8bb6418be5dee7e5232f36296e
jkussoy <s22127003@student.unklab.ac.id>
2025-06-06 12:25:31 +0800

    adding exercise 2: Object Exercises

--- exercise2.js
+++ exercise2.js
@@ -31,6 +31,7 @@
 // By using the dot notation, write the command to add a new key called isFun and a value of true to the programming object.
 // Using a map(), iterate through the languages array and update each element to be “0 - JavaScript, 1 - Python, … ”, where 0 represents the index of the array, and console.log the output.
 
+//Array Exercises
 //1. using for-loop, iterate through this array and console.log all of the people.
 let people = ["Greg", "Mary", "Devon", "James"];
 for (let i = 0; i < people.length; i++) {
@@ -85,3 +86,36 @@ console.log(people);
 //10. Create new variable withBob
 let withBob = people.concat("Bob");
 console.log(withBob);
+
+//Object Exercises
+let programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes:
+    "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke",
+};
+console.log(programming);
+
+// 1. Add "Go" to the end of the languages array.
+programming.languages.push("Go");
+console.log(programming);
+
+// 2. Change the difficulty to 7 using bracket notation.
+programming["difficulty"] = 7;
+console.log(programming);
+
+// 3. Remove the jokes key using delete.
+delete programming.jokes;
+console.log(programming);
+
+// 4. Add new key isFun with value true using dot notation.
+programming.isFun = true;
+console.log(programming);
+
+// 5. Use map() to format and log the languages array.
+let formattedLanguages = programming.languages.map(
+  (lang, index) => `${index} - ${lang}`
+);
+console.log(formattedLanguages);
