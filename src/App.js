f275745373e9a945ff7c2ed05124a342c156729d
jkussoy <s22127003@student.unklab.ac.id>
2025-06-18 23:03:48 +0800

    adding material basic react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Title from "./Title";
+import DisplayArray from "./DisplayArray";
+
+const App = () => {
+  return (
+    // JSX
+    <>
+      <Title />
+      <DisplayArray />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,11 @@
+const Title = () => {
+  const title = "Basic React";
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>Materi tentang pengenalan React JS menggunakan CRA</p>
+    </div>
+  );
+};
+
+export default Title;
